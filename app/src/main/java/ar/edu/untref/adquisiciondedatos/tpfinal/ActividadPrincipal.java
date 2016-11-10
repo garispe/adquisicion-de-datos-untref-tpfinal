363a31c02819334a405bb2c2b296b54f7f488656
garispe <guillermo.arispe@gmail.com>
2016-11-10 19:48:41 -0300

    Se obtienen valores de tomados por los sensores

--- app/src/main/java/ar/edu/untref/adquisiciondedatos/tpfinal/ActividadPrincipal.java
+++ app/src/main/java/ar/edu/untref/adquisiciondedatos/tpfinal/ActividadPrincipal.java
@@ -15,6 +15,8 @@ import ar.edu.untref.adquisiciondedatos.tpfinal.utilidades.OpenGLRenderer;
 
 public class ActividadPrincipal extends AppCompatActivity implements SensorEventListener {
 
+    private static final float ALPHA = 0.3f;
+
     private SensorManager sensorManager;
     private Sensor acelerometro;
     private Sensor magnetometro;
@@ -57,13 +59,60 @@ public class ActividadPrincipal extends AppCompatActivity implements SensorEvent
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
+    private float restringirAngulo(float angulo) {
+        while (angulo >= 180) angulo -= 360;
+        while (angulo < -180) angulo += 360;
+        return angulo;
+    }
 
+    private float calcularDiferencia(float x, float y) {
+
+        float diferencia = x - y;
+
+        //Nos aseguramos que la dif sea <= a 180
+        diferencia = restringirAngulo(diferencia);
+
+        y += ALPHA * diferencia;
+        //Nos aseguramos que Y quede entre [-180 y 180]
+        y = restringirAngulo(y);
+
+        return y;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            gravedad = event.values;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            geomagnetismo = event.values;
+        }
+
+        if (gravedad != null && geomagnetismo != null) {
+
+            float R[] = new float[9];
+            float I[] = new float[9];
+
+            boolean success = SensorManager.getRotationMatrix(R, I, gravedad, geomagnetismo);
+
+            if (success) {
+
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+                nuevoPithc = -(float) ((Math.toDegrees(orientation[1])));
+                nuevoRoll = (float) ((Math.toDegrees(orientation[2])));
+
+                pitch = calcularDiferencia(nuevoPithc, pitch);
+                roll = calcularDiferencia(nuevoRoll, roll);
+            }
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 }
