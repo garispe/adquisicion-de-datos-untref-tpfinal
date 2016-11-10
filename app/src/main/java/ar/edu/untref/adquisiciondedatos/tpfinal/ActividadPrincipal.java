5ff296d057e4eaf0028f6067d766718bde73bfe5
garispe <guillermo.arispe@gmail.com>
2016-11-10 19:25:28 -0300

    Commit inicial

---
+++ app/src/main/java/ar/edu/untref/adquisiciondedatos/tpfinal/ActividadPrincipal.java
@@ -0,0 +1,13 @@
+package ar.edu.untref.adquisiciondedatos.tpfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+    }
+}
