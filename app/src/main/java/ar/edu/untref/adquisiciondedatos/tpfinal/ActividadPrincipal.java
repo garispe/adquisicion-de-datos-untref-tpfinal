55869029e0baf0b46f2e84e763b41e69f288f55e
garispe <guillermo.arispe@gmail.com>
2016-11-10 19:43:40 -0300

    Inicializo los sensores

--- app/src/main/java/ar/edu/untref/adquisiciondedatos/tpfinal/ActividadPrincipal.java
+++ app/src/main/java/ar/edu/untref/adquisiciondedatos/tpfinal/ActividadPrincipal.java
@@ -1,13 +1,69 @@
 package ar.edu.untref.adquisiciondedatos.tpfinal;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class ActividadPrincipal extends AppCompatActivity {
+import ar.edu.untref.adquisiciondedatos.tpfinal.utilidades.OpenGLRenderer;
+
+public class ActividadPrincipal extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor acelerometro;
+    private Sensor magnetometro;
+
+    float[] gravedad;
+    float[] geomagnetismo;
+    float pitch = 0;
+    float roll = 0;
+    float nuevoPithc = 0;
+    float nuevoRoll = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actividad_principal);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometro = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+
+        setContentView(view);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, acelerometro,
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometro,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
